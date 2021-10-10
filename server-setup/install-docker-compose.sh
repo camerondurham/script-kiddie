f7b218c09a3a1e59525e891d2f48db32d3e29ed1
Cameron Durham <camrs@protonmail.com>
2021-10-10 01:06:16 -0700

    Add docker-compose script

---
+++ server-setup/install-docker-compose.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# https://docs.docker.com/compose/install/
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+# install command-line completion
+sudo curl \
+    -L https://raw.githubusercontent.com/docker/compose/1.29.2/contrib/completion/bash/docker-compose \
+    -o /etc/bash_completion.d/docker-compose
+
+docker-compose --version
